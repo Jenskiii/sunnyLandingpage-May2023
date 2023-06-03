e04d4c26f1c56fe914e0dd4b7f8ae2ab99709321
Jenskiii <124568361+Jenskiii@users.noreply.github.com>
2023-06-03 14:02:01 +0200

    landing page

---
+++ main.js
@@ -0,0 +1,30 @@
+const hamburger = document.getElementById("hamburger");
+const menu = document.querySelector(".bubble");
+const fadeIn = document.querySelectorAll(".animation");
+
+hamburger.addEventListener("click", toggleMenu);
+
+
+function toggleMenu() {
+    menu.classList.toggle("active")
+}
+
+// removes menu when item clicked
+document.querySelectorAll(".nav__list").forEach(e => 
+    e.addEventListener("click", () =>{
+        menu.classList.remove("active");   
+    }) )
+
+
+// plays animation when scrolling
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if(entry.isIntersecting) {
+            entry.target.classList.add("play__animation");
+        }else{
+            entry.target.classList.remove("play__animation")
+        }
+    })
+})
+
+fadeIn.forEach((el) => observer.observe(el));;
